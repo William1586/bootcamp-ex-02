f110782328f28c7f1e73f9b5ffe393abdc7b3efb
William Christian <williamcrtl@gmail.com>
2020-06-12 08:24:12 -0300

    User Update - Finished

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    es6: true,
+    node: true,
+  },
+  extends: ['airbnb-base', 'prettier'],
+  plugins: ['prettier'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 11,
+    sourceType: 'module',
+  },
+  rules: {
+    'class-methods-use-this': 'off',
+    'no-param-reassing': 'off',
+    camelcase: 'off',
+    'no-unused-vars': false,
+    'prettier/prettier': 'error',
+    'no-unused-expressions': false,
+  },
+};
--- src/app/middlewares/auth.js
+++ src/app/middlewares/auth.js
@@ -17,9 +17,7 @@ export default async (req, res, next) => {
   try {
     const decoded = await promisify(jwt.verify)(token, authConfig.secret);
 
-    req.userId = decoded.id; 
-
-    console.log(decoded);
+    req.userId = decoded.id;
 
     return next();
   } catch (err) {
